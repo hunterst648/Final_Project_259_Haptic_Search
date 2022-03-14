2bf5e4a5d3e2f147dfe074d315724844e49919f6
hunterst648 <96922792+hunterst648@users.noreply.github.com>
2022-03-14 14:36:16 -0700

    Not cleaned up but this was "Scratch paper" anyway all of the working code is in the markdown.

--- EDA_for_Final.R
+++ EDA_for_Final.R
@@ -27,4 +27,54 @@ ggplot(cond_mean, aes(x=Distractor_size, y= name, color = Distractor_num))+
   geom_line(aes(group = Distractor_num)) + geom_point()
 
 
+# now with accuracy?
 
+cond_meanA <- ds %>%  group_by(Distractor_num, Distractor_size) %>% 
+  summarise_at(vars(Accuracy), list(name = mean))
+
+cond_meanA %>% group_by(Distractor_num)
+ggplot(cond_meanA, aes(x=Distractor_size, y= name, color = Distractor_num))+
+  geom_line(aes(group = Distractor_num)) + geom_point()
+
+Haptic_plots <- function(ds,DV){
+  dep_var <- DV
+  if (dep_var == 1) {
+    cond_meanA <- ds %>%  group_by(Distractor_num, Distractor_size) %>% 
+    summarise_at(vars(Accuracy), list(name = mean))
+    cond_meanA %>% group_by(Distractor_num)
+  
+    p2 <-  ggplot(cond_meanA, aes(x=Distractor_size, y= name, color = Distractor_num))+
+    geom_line(aes(group = Distractor_num)) + geom_point()
+  return(p2)
+  }
+  else {
+    cond_mean <- ds %>%  group_by(Distractor_num, Distractor_size) %>% 
+    summarise_at(vars(`Search_time(s)`), list(name = mean))
+  
+    cond_mean %>% group_by(Distractor_num)
+  
+    p1 <-  ggplot(cond_mean, aes(x=Distractor_size, y= name, color = Distractor_num))+
+    geom_line(aes(group = Distractor_num)) + geom_point()
+  
+  return(p1)
+  }
+}
+
+Haptic_plots(ds,1)
+Haptic_plots(ds,2)
+
+# Try again 
+
+
+Haptic_plots <- function(ds){
+ 
+    cond_meanA <- ds %>%  group_by(Distractor_num, Distractor_size) %>% 
+      summarise_at(vars(Accuracy), list(name = mean))
+    cond_meanA %>% group_by(Distractor_num)
+    
+    p2 <-  ggplot(cond_meanA, aes(x=Distractor_size, y= name, color = Distractor_num))+
+      geom_line(aes(group = Distractor_num)) + geom_point()
+    return(p2)
+    
+}
+Haptic_plots(ds)
